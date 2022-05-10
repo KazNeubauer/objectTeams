0bc2d75fd7ac030673e6777b7946d4da743b84a7
Kaz Neubauer <93604020+KazNeubauer@users.noreply.github.com>
2022-05-10 15:30:48 +0000

    add a comment in Gama

--- Game.java
+++ Game.java
@@ -1,5 +1,5 @@
 public interface Game{
 
-  //have user input to determine # of players or have players manually opt in
+  //have user input to determine # of players or have players manually opt in.
   public Game joinGame(Player[] players);
 }
